8ef56300f130cf74711ebf093406aa29ffe01f79
ZerozeroCom <zerozerocomputer@gmail.com>
2022-01-23 12:39:59 +0800

    套版ok

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -7,43 +7,46 @@
 --}}
 @extends('layouts.admin_app')
 @section('content')
-<h2>後臺訂單列表</h2>
-<span>訂單總數:{{ $orderCount}} </span>
-<a href="/admin/orders/excel/export">匯出訂單excel</a>
-<a href="/admin/orders/excel/export-by-shipped">匯出分類訂單excel</a>
-<table>
-<thead>
-    <tr>
-        <td>購買時間</td>
-        <td>購買者</td>
-        <td>商品清單</td>
-        <td>訂單總額</td>
-        <td>是否運送</td>
-    </tr>
-</thead>
-<tbody>
-    @foreach ($orders as $order)
-    <tr>
-        <td>{{$order ->created_at}}</td>
-        {{--order Models 內有user方法取得user--}}
-        <td>{{$order ->user->name}}</td>
-        <td>
-            @foreach ($order->orderItems as $orderItem)
-                {{$orderItem->product->title }} &nbsp;
-            @endforeach
-        </td>
-        <td>{{ isset($order->orderItems) ? $order->orderItems->sum('price') : 0}}</td>
-        <td>{{$order->is_shipped}}</td>
-    </tr>
-    @endforeach
-</tbody>
-
-</table>
-<div>
-    @for ($i = 1; $i <= $orderPages; $i++)
-        <a href="/admin/orders?page={{ $i }}">第{{$i}}頁</a> &nbsp;
-    @endfor
-
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">後臺訂單列表</h1>
+    <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+            class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
 </div>
-
+<div class="row">
+    <span>訂單總數:{{ $orderCount}} </span>
+    <a href="/admin/orders/excel/export">匯出訂單excel</a>
+    <a href="/admin/orders/excel/export-by-shipped">匯出分類訂單excel</a>
+    <table class="table">
+    <thead>
+        <tr>
+            <td>購買時間</td>
+            <td>購買者</td>
+            <td>商品清單</td>
+            <td>訂單總額</td>
+            <td>是否運送</td>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($orders as $order)
+        <tr>
+            <td>{{$order ->created_at}}</td>
+            {{--order Models 內有user方法取得user--}}
+            <td>{{$order ->user->name}}</td>
+            <td>
+                @foreach ($order->orderItems as $orderItem)
+                    {{$orderItem->product->title }} &nbsp;
+                @endforeach
+            </td>
+            <td>{{ isset($order->orderItems) ? $order->orderItems->sum('price') : 0}}</td>
+            <td>{{$order->is_shipped}}</td>
+        </tr>
+        @endforeach
+    </tbody>
+    </table>
+    <div>
+        @for ($i = 1; $i <= $orderPages; $i++)
+            <a href="/admin/orders?page={{ $i }}">第{{$i}}頁</a> &nbsp;
+        @endfor
+    </div>
+</div>
 @endsection
--- resources/views/layouts/admin_app.blade.php
+++ resources/views/layouts/admin_app.blade.php
@@ -1,7 +1,10 @@
-<html>
+<!DOCTYPE html>
+<html lang="tw">
     <head>
+        <meta charset="utf-8">
         <title>電商管理網站</title>
-        <link rel="stylesheet" href="/css/bootstrap.min.css">
+
+        <link rel="stylesheet" href="/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn">
         <script src="/js/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="></script>
         <script src="/js/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"></script>
         <script src="/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2"></script>
@@ -9,12 +12,33 @@
         <script src="/js/jquery.dataTables.js" ></script>
         <link rel="stylesheet" href="/css/app.css">
 
+     <!-- Custom fonts for this template-->
+        <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="/css/sb-admin-2.min.css" rel="stylesheet">
+
     </head>
-    <body>
-        @include('layouts.admin_nav')
-        <div>
-            @yield('content')
+    <body id="page-top">
+        <div id="wrapper">
+
+            @include('layouts.admin_nav')
+
+            <div id="content-wrapper" class="d-flex flex-column p-3 mt-3">
+                        @yield('content')
+                    <footer class="sticky-footer bg-white">
+                        <div class="container my-auto">
+                            <div class="copyright text-center my-auto">
+                                <span>Copyright &copy; 電商管理網站 2021</span>
+                            </div>
+                        </div>
+                    </footer>
+
+            </div>
         </div>
+        <a class="scroll-to-top rounded" href="#page-top">
+
     </body>
     @stack('scripts')
 </html>
